a79ce6a40e392a6ffa529e4941e85b983489a79e
jharveyb <jharveyb@mit.edu>
2016-12-27 03:19:14 -0500

    Fixed record/statistics, ready to parallelize

--- map.go
+++ map.go
@@ -26,24 +26,13 @@ type Hashmap struct {
 	hashmap []*cmap.ConcurrentMap
 }
 
-var Updater cmap.UpsertCb
-var Recordater cmap.UpsertCb
-
 // Calculates the total # of hashes & prints useful info.
 
-func (h Hashmap)  Diagnostic() int {
+func (h Hashmap) Diagnostic() int {
 	sum := 0
-	/*
-	for i := uint(0); i < h.cols; i++ {
-		inc, ok := h.record.Get(strconv.FormatInt(int64(i), 10))
-		//fmt.Println(inc)
-		if ok != false {
-			incmap := inc.(MapEntry)
-			sum += incmap.Count
-		}
+	for _, val := range h.record {
+		sum += val
 	}
-	*/
-	sum = int(h.cols)
 	if h.debug == true {
 		fmt.Println("Diff is", h.diff)
 		fmt.Println("Cols is", h.cols)
@@ -70,13 +59,11 @@ func (h Hashmap) Update() {
 	// desired behavior is to set in first map,
 	// and use that entry as pointer to its most recent
 	// position
-	fmt.Println("Begin Update")
 	flg := false
 	absent := false
 	entry := "test"
 	for flg == false {
 		entry = RandString(h.ceiling)
-		//fmt.Println(entry)
 		absent = h.hashmap[0].SetIfAbsent(entry, 0)
 		if absent == false {
 			ind, _ := h.hashmap[0].Get(entry)
@@ -93,9 +80,7 @@ func (h Hashmap) Update() {
 				dubsum := 0
 				for i := range h.hashmap {
 					hitcount := h.hashmap[i].Count()
-					//fmt.Println(hitcount)
 					h.record[i] = hitcount
-					//fmt.Println(h.hashmap[i].Items())
 					if i != 0 {
 						dubsum += hitcount
 					}
@@ -105,69 +90,6 @@ func (h Hashmap) Update() {
 			}
 		}
 	}
-	/*
-	Updater = func(state bool, val interface{}, inpval interface{}) (out interface{}) {
-		var outmap MapEntry
-		var valmap MapEntry
-		if state == true {
-			valmap = val.(MapEntry)
-			outmap.Count = valmap.Count + 1
-			if uint(outmap.Count) == h.cols {
-				if h.debug == true {
-					fmt.Println("Desired collisions found!")
-				}
-				outmap.State = true
-			}
-		} else {
-			outmap.Count = 1
-		}
-		out = outmap
-		return out
-	}
-	*/
-	/*
-	Recordater = func(state bool, val interface{}, inpval interface{}) (out interface{}) {
-		var outmap MapEntry
-		outmap.Count = 1
-		if state == true {
-			outmap.Count += inpval.(int)
-		}
-		out = outmap
-		return out
-	}
-	*/
-	/*
-	sol := h.hashmap.Upsert(entry, h.cols, Updater)
-	solmap := sol.(MapEntry)
-	if solmap.State == true {
-		return true
-	} else { return false }
-	*/
-	/*
-	else {
-		ind := solmap.Count - 1
-		h.record.Upsert(strconv.FormatInt(int64(ind), 10), ind, Recordater)
-		return false
-	}
-	*/
-	/*
-	ind, ok := h.hashmap[entry]
-	if ok == true {
-		h.record[ind] += 1
-		h.hashmap[entry] += 1
-		if h.hashmap[entry] == h.cols {
-			if h.debug == true {
-				fmt.Println("Desired collisions found!")
-			}
-			return true
-		}
-	}
-	if ok == false {
-		h.hashmap[entry] = 1
-		h.record[0] += 1
-	}
-	return false
-	*/
 }
 
 // Creates a hashmap & solves, returning the total # of hashes.
